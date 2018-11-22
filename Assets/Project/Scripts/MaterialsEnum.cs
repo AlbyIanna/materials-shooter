39b64e9ee74e2c73654a41b567dfd89932112b97
Alberto Iannaccone <alby.ianna@gmail.com>
2018-11-23 00:43:57 +0100

    change cubes color based on the selected material

---
+++ Assets/Project/Scripts/MaterialsEnum.cs
@@ -0,0 +1,1 @@
+﻿public enum Material { Earth, Grass, Sticky };
---
+++ Assets/Project/Scripts/SelectMaterial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectMaterial : MonoBehaviour {
+    public Image crosshair;
+    public Dictionary<Material, Color> materialsColor = new Dictionary<Material, Color>();
+    public Material selectedMaterial;
+
+
+    // Use this for initialization
+    void Start () {
+        materialsColor.Add(Material.Earth, new Color(.55f, .3f, .1f));
+        materialsColor.Add(Material.Grass, new Color(0, .5f, 0));
+        materialsColor.Add(Material.Sticky, new Color(.8f, .8f, 0));
+        selectedMaterial = Material.Earth;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedMaterial = Material.Earth;
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedMaterial = Material.Grass;
+        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedMaterial = Material.Sticky;
+        }
+
+        crosshair.color = materialsColor[selectedMaterial];
+    }
+}
