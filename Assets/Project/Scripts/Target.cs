8694a8141284d476f144173051076d7b39200e55
Alberto Iannaccone <alby.ianna@gmail.com>
2018-11-24 02:33:15 +0100

    shoot materials

--- Assets/Project/Scripts/Target.cs
+++ Assets/Project/Scripts/Target.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 	}
 	
 	public void HitTarget(Color color) {
-		GetComponent<MeshRenderer> ().material.color = color;
+		GetComponent<MeshRenderer>().material.color = color;
 		Destroy (gameObject, 1);
 	}
 
